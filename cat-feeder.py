b239ab9d8dd24e30cf73d406ec5b5940ce92d073
Lawrence Foley <emwav333@gmail.com>
2017-07-29 23:45:39 -0500

    Changed LED pulse

--- cat-feeder.py
+++ cat-feeder.py
@@ -2,13 +2,14 @@ from utils import feed
 import schedule
 from time import sleep
 import logging
+from gpiozero import PWMLED
 
 if __name__ == "__main__":
     logging.basicConfig(filename='cat-feeder.log', level=logging.INFO, format='%(asctime)s | %(module)s | %(message)s')
     logging.info("Starting up...")
 
     led = PWMLED(4)
-    led.blink(on_time=1, off_time=1, fade_in_time=3, fade_out_time=3, n=None, background=True)
+    led.blink(on_time=0, off_time=5, fade_in_time=0.25, fade_out_time=0.25, n=None, background=True)
 
     # Morning
     schedule.every().day.at("09:00").do(feed, quarterCups=1)
