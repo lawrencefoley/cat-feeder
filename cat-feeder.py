7438242af2579f3cf4659d7932575d9709f86cab
Lawrence Foley <emwav333@gmail.com>
2017-07-29 22:58:11 -0500

    Adjusted time. Added gpiozero requirement.

--- cat-feeder.py
+++ cat-feeder.py
@@ -4,14 +4,14 @@ from time import sleep
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='cat-feeder.log', level=logging.DEBUG)
+    logging.basicConfig(filename='cat-feeder.log', level=logging.INFO, format='%(asctime)s | %(module)s | %(message)s')
     logging.info("Starting up...")
 
     # Morning
-    schedule.every().day.at("09:00").do(feed)
+    schedule.every().day.at("09:00").do(feed, quarterCups=1)
 
     # Evening
-    schedule.every().day.at("22:09").do(feed)
+    schedule.every().day.at("22:45").do(feed, quarterCups=1)
 
     #schedule.every().wednesday.at("13:15").do(job)
 
